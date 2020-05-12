7f264d874a218a0180adf463c14f43218797315f
sigmade <sychyov1991@mail.ru>
2020-05-12 09:42:29 +0300

    Created Table

---
+++ leave-management/Data/LeaveAllocation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace leave_management.Data
+{
+    public class LeaveAllocation
+    {
+        [Key]
+        public int Id { get; set; }
+        public string NumberOfDays { get; set; }
+        public DateTime DateCreated { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+        public string EmployeeId { get; set; }
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+    }
+}
