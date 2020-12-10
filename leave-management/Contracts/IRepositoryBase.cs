0b55c7d8723f3812eff738578cf8a26e4a5dbc7b
sigmade <sychyov1991@mail.ru>
2020-12-10 21:19:20 +0300

    added IRepositoryBase

---
+++ leave-management/Contracts/IRepositoryBase.cs
@@ -0,0 +1,30 @@
+﻿using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Contracts
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        ICollection<T> FindAll();
+        T FindById(int id);
+        bool Create(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+        bool Save();
+    }
+    public interface ILeaveHistoryRepository : IRepositoryBase<LeaveHistory>
+    {
+    }
+
+    public interface ILeaveAllocationRepository : IRepositoryBase<LeaveAllocation>
+    {
+    }
+
+    public interface ILeaveTypeRepository : IRepositoryBase<LeaveType>
+    {
+        ICollection<LeaveType> GetEmployeesByLeaveType(int id);
+    }
+}
